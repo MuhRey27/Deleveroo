11aa131bdc7661a9bd1163e388aa0fbecd738225
Muh Rey <mrcristal.bo@gmail.com>
2021-10-10 00:47:29 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Menu
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".nav-menu");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
