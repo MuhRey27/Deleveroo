2c2d67a8d4b0cab9412e224837427db1a8919217
Muh Rey <mrcristal.bo@gmail.com>
2021-10-10 02:13:12 +0700

    Update

--- js/app.js
+++ js/app.js
@@ -4,4 +4,39 @@ const navbar = document.querySelector(".nav-menu");
 
 menu.addEventListener("click", () => {
   navbar.classList.toggle("active");
+  const link = document.querySelectorAll(".nav-item");
+  link.forEach((e) => {
+    e.addEventListener("click", () => {
+      navbar.classList.remove("active");
+    });
+  });
+});
+
+// Loading Menu
+const loader = document.querySelector(".preloader");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 3000);
+});
+
+const scrollTop = document.querySelector(".scrool-top");
+
+scrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", (e) => {
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > 300) {
+    scrollTop.classList.add("show");
+  } else {
+    scrollTop.classList.remove("show");
+  }
 });
